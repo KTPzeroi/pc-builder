b980f4cf8731da8ec075ca5c9ae238bde790d7ff
POTTER <167728801+Punnpong@users.noreply.github.com>
2026-02-09 18:07:11 +0700

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+//home//
 "use client"
 import Image from "next/image";
 import {Button} from "@heroui/react";
@@ -5,7 +6,7 @@ import {Button} from "@heroui/react";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-slate-800">
-      <h1 className="text-blue-500 text-3xl font-bold">Hero Section</h1>
+      <h1 className="text-blue-500 text-3xl font-bold">VALORNT</h1>
       
     </main>
 
